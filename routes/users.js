d91d2bde5c0622a55e881b0c49c0021c4c17af20
Ali <idris1390@gmail.com>
2024-05-02 10:50:24 +0200

    test

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
+require('../models/connections');
+const User = require('../models/users');
+const { checkBody } = require('../modules/checkBody');
+const uid2 = require('uid2');
+const bcrypt = require('bcrypt');
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/signup', function(req, res,) {
+  if (!checkBody(req.body, ['username', 'password'])) {
+    res.json({ result: false, error: 'Missing or empty fields' });
+    return;
+   }
+User.findOne({firstname: req.body.username,firstname: req.body.username }).then(data => {
+  if (data === null){
+    const hash = bcrypt.haschSync(req.body.password,10);
+
+  const newUser = new User ({
+    firstname: req.body.firstname,
+    username: req.body.username,
+    password: hash,
+    token: uid2(32),
+  })
+  newUser.save().then(dataUser => {
+    res.json({ result:true, token: userData.token});
+  });
+  }else {
+    res.json({ result :false, error: 'User already exists'});
+  }
+})
 });
 
 module.exports = router;
